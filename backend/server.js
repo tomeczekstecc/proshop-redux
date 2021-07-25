95869a603d35fc3bb7b50dbccc5b603f4889877d
Tomasz Steć <tomeczekstecc@gmail.com>
2021-07-25 20:40:38 +0200

    add

--- backend/server.js
+++ backend/server.js
@@ -2,27 +2,33 @@ import express from 'express';
 import dotenv from 'dotenv';
 import products from './data/products.js';
 import connectDB from './config/db.js';
-import colors from 'colors'
+import colors from 'colors';
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 const app = express();
 
 connectDB();
+app.use((req, res, next) => {
+  console.log('Hello');
+  next();
+});
+
 
 app.get('/', (req, res) => {
   res.send('API is running');
 });
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+app.use(errorHandler);
+app.use(notFound);
 app.listen(
   5000,
-  console.log(`Server started in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server started in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
