3835bb864c3a3f36ae387a16dd217e64c406d0de
Tomasz Steć <tomeczekstecc@gmail.com>
2021-09-12 22:55:49 +0200

    fix

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -20,6 +20,7 @@ router.get(
   '/:id',
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
+
     if (product) {
       res.json(product);
     } else {
--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 connectDB();
 app.use((req, res, next) => {
-  console.log('Hello');
+
   next();
 });
 
